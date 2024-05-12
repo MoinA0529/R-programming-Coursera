b5b88d3d11a05623d1c2d728db191daa316f36a7
Moin Ahmed <63891600+MoinA0529@users.noreply.github.com>
2024-05-12 13:37:44 +0530

    Update Ignore.java

--- Ignore.java
+++ Ignore.java
@@ -1,25 +1,20 @@
-@Test
-    void testUserEntityManager() {
-        // Arrange
-        when(config.userDataSource()).thenReturn(dataSource);
-        
-        // Act
-        LocalContainerEntityManagerFactoryBean result = config.userEntityManager();
-        
-        // Assert
-        assertNotNull(result, "EntityManagerFactoryBean should not be null");
-        assertNotNull(result.getDataSource(), "DataSource should not be null");
-        assertNotNull(result.getJpaVendorAdapter(), "JpaVendorAdapter should not be null");
-    }
-
-    @Test
-    void testUserTransactionManager() {
-        // Arrange
-        when(config.userEntityManager()).thenReturn(new LocalContainerEntityManagerFactoryBean());
-        
-        // Act
-        PlatformTransactionManager result = config.userTransactionManager();
-        
-        // Assert
-        assertNotNull(result, "TransactionManager should not be null");
-    }
+List<RepCodeRelationItem> repCodeRelationItems = new ArrayList<>();
+        for (RepCodeOwnersSearchResponse response : responseDetails.getData()) {
+            RepCodeRelationItem item = new RepCodeRelationItem();
+            item.setRepCode(response.getRepCode());
+            item.setSubFirm(response.getSubFirm());
+            item.setBranch(response.getBranch());
+            
+            List<TeamMemberRelationItem> memberItems = new ArrayList<>();
+            for (MemberData memberData : response.getMembers()) {
+                TeamMemberRelationItem memberItem = new TeamMemberRelationItem();
+                memberItem.setName(memberData.getName());
+                memberItem.setShowToClient(memberData.isShowToClient());
+                memberItem.setIndex(memberData.getIndex());
+                memberItem.setReceiveEmail(memberData.isReceiveEmail());
+                memberItem.setEmployeeId(memberData.getEmployeeId());
+                memberItem.setPpid(memberData.getPpid());
+                memberItem.setStatus(memberData.getStatus());
+                memberItems.add(memberItem);
+            }
+            item.setMembers(memberItems);
