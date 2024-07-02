32089f718a18ae5ba32182c5189abcd39e0b5b2a
Moin Ahmed <63891600+MoinA0529@users.noreply.github.com>
2024-07-02 12:51:32 +0530

    Update Ignore.java

--- Ignore.java
+++ Ignore.java
@@ -1,83 +1,66 @@
 import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
-import { reshareService } from './reshareService';
-import { BASE_URL_FOR_SAVE } from "../components/utils/constants";
+import { reshareService } from '../save-reshare-service';
+import { SAVE_FILE_URL } from "../../components/utils/constants";
+
+jest.mock('axios');
 
 describe('reshareService', () => {
-  let mock;
+  const mockDocumentObject = {
+    icmpDocId: '123',
+    icmpRepoId: '456',
+    category: 'Test',
+    filename: 'test.pdf',
+    filesize: '1024',
+    createdBy: 'user1',
+    attestation: true
+  };
+
+  const mockRepCodeOwnersSelected = {
+    data: [
+      { docSpaceId: 'space1', selected: true },
+      { docSpaceId: 'space2', selected: false },
+      { docSpaceId: 'space3', selected: true }
+    ]
+  };
 
   beforeEach(() => {
-    mock = new MockAdapter(axios);
-  });
-
-  afterEach(() => {
-    mock.reset();
+    axios.post.mockClear();
   });
 
   it('should make POST requests for each selected docSpaceId', async () => {
-    const documentObject = {
-      icmpDocId: '123',
-      icmpRepoId: '456',
-      category: 'Test',
-      filename: 'test.pdf',
-      filesize: '1024',
-      createdBy: 'user1',
-      attestation: true
-    };
+    axios.post.mockResolvedValue({ data: 'Success' });
 
-    const repCodeOwnersSelected = {
-      data: [
-        { docSpaceId: 'space1', selected: true },
-        { docSpaceId: 'space2', selected: false },
-        { docSpaceId: 'space3', selected: true }
-      ]
-    };
+    await reshareService(mockDocumentObject, mockRepCodeOwnersSelected);
 
-    mock.onPost(`${BASE_URL_FOR_SAVE}/document/save`).reply(200);
-
-    await reshareService(documentObject, repCodeOwnersSelected);
-
-    expect(mock.history.post.length).toBe(2);
-
-    expect(mock.history.post[0].url).toBe(`${BASE_URL_FOR_SAVE}/document/save`);
-    expect(JSON.parse(mock.history.post[0].data)).toEqual({
-      ...documentObject,
-      docSpaceId: 'space1'
-    });
-
-    expect(mock.history.post[1].url).toBe(`${BASE_URL_FOR_SAVE}/document/save`);
-    expect(JSON.parse(mock.history.post[1].data)).toEqual({
-      ...documentObject,
-      docSpaceId: 'space3'
-    });
-
-    mock.history.post.forEach(request => {
-      expect(request.headers['Content-Type']).toBe('application/json');
-    });
+    expect(axios.post).toHaveBeenCalledTimes(2);
+    expect(axios.post).toHaveBeenNthCalledWith(1, SAVE_FILE_URL, 
+      { ...mockDocumentObject, docSpaceId: 'space1' },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+    expect(axios.post).toHaveBeenNthCalledWith(2, SAVE_FILE_URL, 
+      { ...mockDocumentObject, docSpaceId: 'space3' },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
   });
 
   it('should not make any requests if no docSpaceIds are selected', async () => {
-    const documentObject = { /* ... */ };
-    const repCodeOwnersSelected = {
+    const noSelectedRepCodeOwners = {
       data: [
         { docSpaceId: 'space1', selected: false },
         { docSpaceId: 'space2', selected: false }
       ]
     };
 
-    await reshareService(documentObject, repCodeOwnersSelected);
+    await reshareService(mockDocumentObject, noSelectedRepCodeOwners);
 
-    expect(mock.history.post.length).toBe(0);
+    expect(axios.post).not.toHaveBeenCalled();
   });
 
   it('should throw an error if the API request fails', async () => {
-    const documentObject = { /* ... */ };
-    const repCodeOwnersSelected = {
-      data: [{ docSpaceId: 'space1', selected: true }]
-    };
+    const mockError = new Error('Network Error');
+    axios.post.mockRejectedValue(mockError);
 
-    mock.onPost(`${BASE_URL_FOR_SAVE}/document/save`).reply(500);
-
-    await expect(reshareService(documentObject, repCodeOwnersSelected)).rejects.toThrow();
+    await expect(reshareService(mockDocumentObject, mockRepCodeOwnersSelected)).rejects.toThrow('Network Error');
+    expect(axios.post).toHaveBeenCalledTimes(1);
   });
 });
